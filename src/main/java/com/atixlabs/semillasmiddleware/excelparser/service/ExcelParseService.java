2a3ca5d822e5e7ca21c91d54f495e3eba4576ccc
Luciana Colombo <lcolombo@atixlabs.com>
2020-03-25 09:55:03 -0300

    feat(parser): Added abstract ExcelParseService with abstract method processRow()
    Method processSingleSheetFile keeps processing rows while the surveyId is the same. It considers the changes of categories too

--- src/main/java/com/atixlabs/semillasmiddleware/excelparser/service/ExcelParseService.java
+++ src/main/java/com/atixlabs/semillasmiddleware/excelparser/service/ExcelParseService.java
@@ -1,24 +1,44 @@
 package com.atixlabs.semillasmiddleware.excelparser.service;
 
+import com.atixlabs.semillasmiddleware.excelparser.app.CategoryExcelFileService;
+import com.atixlabs.semillasmiddleware.excelparser.app.CategoryServiceFactory;
+import com.atixlabs.semillasmiddleware.excelparser.app.SurveyExcelFileService;
 import com.atixlabs.semillasmiddleware.excelparser.dto.ProcessExcelFileResult;
+import com.atixlabs.semillasmiddleware.excelparser.exception.InvalidRowException;
 import com.atixlabs.semillasmiddleware.filemanager.util.FileUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Iterator;
 
 @Service
 @Slf4j
-public class ExcelParseService {
+public abstract class ExcelParseService {
+
+    String surveyName;
+    String category;
 
     @Autowired
     FileUtil fileUtil;
 
-    public ProcessExcelFileResult validateFile(String filePath) throws FileNotFoundException {
+    /**
+     *
+     * Levanta el archivo y lee linea por linea
+     *
+     * @param filePath
+     * @return
+     * @throws FileNotFoundException
+     */
+    public ProcessExcelFileResult processSingleSheetFile(String filePath) throws IOException, InvalidRowException {
+        String currentSurveyName;
+        String currentCategory;
+
         log.info("Validation for file "+filePath+" begins");
 
         File xlsxFile = fileUtil.getFileByPath(filePath);
@@ -26,8 +46,60 @@ public class ExcelParseService {
         ProcessExcelFileResult processExcelFileResult = new ProcessExcelFileResult();
         processExcelFileResult.setFileName(filePath);
 
-        InputStream inputStream = new FileInputStream(xlsxFile);
+        FileInputStream fileInput = new FileInputStream(xlsxFile);
+
+        Workbook workbook = new XSSFWorkbook(fileInput);
+
+        Sheet sheet = workbook.getSheetAt(0);
+        Iterator<Row> rowsIterator = sheet.rowIterator();
+
+        if (rowsIterator.hasNext())
+            rowsIterator.next();
+
+        //process survey specific data
+        if (rowsIterator.hasNext()){
+            Row currentRow = rowsIterator.next();
+            SurveyExcelFileService surveyExcelFileService = new SurveyExcelFileService();
+            surveyExcelFileService.processRow(currentRow, processExcelFileResult);
+            surveyName = getSurveyName(currentRow);
+            category = getCategory(currentRow);
+
+            currentRow = rowsIterator.next();
+            currentSurveyName = getSurveyName(currentRow);
+            currentCategory = getCategory(currentRow);
+
+            while (rowsIterator.hasNext()) {
+                while (rowsIterator.hasNext() && surveyName == currentSurveyName) {
+                    while (rowsIterator.hasNext() && surveyName == currentSurveyName && category == currentCategory) {
+                        CategoryServiceFactory categoryServiceFactory = new CategoryServiceFactory();
+                        CategoryExcelFileService categoryService = categoryServiceFactory.create(category);
+                        categoryService.processRow(currentRow, processExcelFileResult);
+
+                        currentRow = rowsIterator.next();
+                        currentCategory = getCategory(currentRow);
+                        currentSurveyName = getSurveyName(currentRow);
+                    }
+                    category = currentCategory;
+                }
+                surveyName = currentSurveyName;
+            }
+
+            //if processExcelFileResult no tiene errores --> crear credenciales
+        }
 
         return processExcelFileResult;
     }
+
+    private String getSurveyName(Row row){
+        return row.getCell(7).getStringCellValue();
+    }
+
+    private String getCategory(Row row){
+        return row.getCell(14).getStringCellValue();
+    }
+
+
+    public abstract void processRow(Row row, ProcessExcelFileResult processExcelFileResult) throws InvalidRowException;
+
+
 }

bb3df00260539bd2f6927cfb70d8325d39dfec57
Luciana Colombo <lcolombo@atixlabs.com>
2020-03-26 15:27:49 -0300

    feat(parser): updated category service factory with recently added categories

--- src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/service/CategoryServiceFactory.java
+++ src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/service/CategoryServiceFactory.java
@@ -2,11 +2,32 @@ package com.atixlabs.semillasmiddleware.excelparser.app.service;
 
 public class CategoryServiceFactory {
     public CategoryExcelFileService create(String category){
-        switch (category.toUpperCase()){
+        //Removes numbers in category name to reduce the number of alternatives (i.e: DATOS HIJO 1, DATOS HIJO 2, etc)
+        switch (category.toUpperCase().replaceAll("[0123456789]","")) {
             case "EMPRENDIMIENTO":
                 return new EntrepreneurshipExcelFileService();
             case "DATOS DEL BENEFICIARIO":
                 return new BeneficiaryExcelFileService();
+            case "OTROS INGRESOS FAMILIARES":
+                return new OtherFamilyIncomesExcelFileService();
+            case "VIVIENDA":
+                return new DwellingExcelFileService();
+            case "DATOS ENTREVISTA":
+                return new InterviewExcelFileService();
+            case "FOTOS ADICIONALES":
+                return new PicturesExcelFileService();
+            case "SITUACIÓN PATRIMONIAL":
+                return new PatrimonialSituationExcelFileService();
+            case "FINANZAS FAMILIARES":
+                return new FamilyFinancesExcelFileService();
+            case "INGRESOS SOLICITANTE":
+                return new ApplicantIncomesExcelFileService();
+            case "DATOS DEL CONYUGE":
+                return new KinsmanExcelFileService("Conyuge");
+            case "DATOS HIJO":
+                return new KinsmanExcelFileService("Hijo");
+            case "OTRO MIEMBRO DE LA FAMILIA":
+                return new KinsmanExcelFileService(null);
         }
         return null;
     }

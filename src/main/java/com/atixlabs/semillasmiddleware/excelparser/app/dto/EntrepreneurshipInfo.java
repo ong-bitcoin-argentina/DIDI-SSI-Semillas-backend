ae816adab5bb2e5fa771bf3455bb4c88cd7af40a
Luciana Colombo <lcolombo@atixlabs.com>
2020-03-27 16:21:39 -0300

    feat(parser-entrepreunership): EntrepreunershipType must be Enum instead of 'string'

--- src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/dto/EntrepreneurshipInfo.java
+++ src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/dto/EntrepreneurshipInfo.java
@@ -1,5 +1,6 @@
 package com.atixlabs.semillasmiddleware.excelparser.app.dto;
 
+import com.atixlabs.semillasmiddleware.excelparser.app.dto.enums.EntrepreunershipTypeEnum;
 import com.atixlabs.semillasmiddleware.excelparser.exception.InvalidQuestionException;
 import com.atixlabs.semillasmiddleware.excelparser.exception.InvalidRowException;
 import org.apache.poi.ss.usermodel.Row;
@@ -10,7 +11,7 @@ import java.time.LocalDate;
 
 public class EntrepreneurshipInfo extends ExcelRow {
     private static Map<String, Class<?>> QUESTIONS = new HashMap<>() {{
-        put("Tipo de emprendimiento", String.class);
+        put("Tipo de emprendimiento", EntrepreunershipTypeEnum.class);
         put("Actividad principal", String.class);
         put("Nombre emprendimiento", String.class);
         put("Dirección", String.class);
@@ -27,11 +28,10 @@ public class EntrepreneurshipInfo extends ExcelRow {
         String question = getCellStringValue(row,15,"Emprendimiento pregunta");
         if (answers.containsKey(question)){
             answers.put(question,getCellStringValue(row,16,"Emprendimiento respuesta"));
-            //IGNORAR TITULOS! EJEMPLO: "Ingresos y Egresos", "Datos del emprendimiento"
         }
-        else {
-            throw new InvalidQuestionException(question);
-        }
+    //    else {
+            //throw new InvalidQuestionException(question);
+    //    }
     }
 
     @Override
---
+++ src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/dto/enums/EntrepreunershipTypeEnum.java
@@ -0,0 +1,7 @@
+package com.atixlabs.semillasmiddleware.excelparser.app.dto.enums;
+
+public enum EntrepreunershipTypeEnum {
+    COMERCIO,
+    PRODUCCION,
+    SERVICIO
+}

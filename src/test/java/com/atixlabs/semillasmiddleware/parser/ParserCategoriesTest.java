a1e4d717917626e1c80a9229edb0a983df976d1a
Luciana Colombo <lcolombo@atixlabs.com>
2020-04-07 18:40:02 -0300

    feat(parser): added test

--- src/test/java/com/atixlabs/semillasmiddleware/parser/ParserCategoriesTest.java
+++ src/test/java/com/atixlabs/semillasmiddleware/parser/ParserCategoriesTest.java
@@ -1,9 +1,15 @@
 package com.atixlabs.semillasmiddleware.parser;
 
 import com.atixlabs.semillasmiddleware.excelparser.app.categories.AnswerCategoryFactory;
+import com.atixlabs.semillasmiddleware.excelparser.app.categories.Category;
+import com.atixlabs.semillasmiddleware.excelparser.app.categories.PersonCategory;
+import com.atixlabs.semillasmiddleware.excelparser.app.constants.PersonType;
 import com.atixlabs.semillasmiddleware.excelparser.exception.InvalidCategoryException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.stubbing.Answer;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class ParserCategoriesTest {
     @Test
@@ -16,6 +22,16 @@ public class ParserCategoriesTest {
     public void invalidCategoryThrowsInvalidCategoryException() throws InvalidCategoryException, Exception {
         AnswerCategoryFactory answerCategoryFactory = new AnswerCategoryFactory();
         answerCategoryFactory.get("non-existent category");
+    }
+
+    @Test
+    public void categoryFactoryAssignsChildItsCorrespondingEnum() throws Exception, InvalidCategoryException {
+        AnswerCategoryFactory categoryFactory = new AnswerCategoryFactory();
+        PersonCategory child = (PersonCategory) categoryFactory.get("DATOS HIJO 1");
+        Assert.assertEquals(child.getPersonType(), PersonType.CHILD);
+
+        PersonCategory spouse = (PersonCategory) categoryFactory.get("DATOS DEL CÓNYUGE");
+        Assert.assertEquals(spouse.getPersonType(), PersonType.SPOUSE);
     }
 
 }

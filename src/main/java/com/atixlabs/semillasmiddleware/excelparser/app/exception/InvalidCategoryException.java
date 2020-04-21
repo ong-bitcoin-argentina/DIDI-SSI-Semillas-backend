285ed5c8da232745bed2b8e82327bb910d19413d
Luciana Colombo <lcolombo@atixlabs.com>
2020-04-21 11:31:12 -0300

    fix(parser): Exceptions inherit from Exception not from Throwable

--- src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/exception/InvalidCategoryException.java
+++ src/main/java/com/atixlabs/semillasmiddleware/excelparser/app/exception/InvalidCategoryException.java
@@ -1,6 +1,6 @@
 package com.atixlabs.semillasmiddleware.excelparser.app.exception;
 
-public class InvalidCategoryException extends Throwable {
+public class InvalidCategoryException extends Exception {
     public InvalidCategoryException(String invalidCategory){
         super("No existe la categoría: " + invalidCategory);
     }

a4165df6969f3b48f25325567e56ab940d5fb33f
uribrama <uribrama@gmail.com>
2020-05-28 09:39:46 -0300

    fix: origins for credentials

--- src/main/java/com/atixlabs/semillasmiddleware/app/controller/CredentialController.java
+++ src/main/java/com/atixlabs/semillasmiddleware/app/controller/CredentialController.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(CredentialController.URL_MAPPING_CREDENTIAL)
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.OPTIONS})
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.PATCH})
 @Slf4j
 public class CredentialController {
 

2b65ca24db2222f794d9f56c5880937b220b257c
Martín Luoni <mluoni@atixlabs.com>
2020-07-02 09:58:33 -0300

    feat(credential): add filter holder for search

--- src/main/java/com/atixlabs/semillasmiddleware/app/controller/CredentialController.java
+++ src/main/java/com/atixlabs/semillasmiddleware/app/controller/CredentialController.java
@@ -51,7 +51,7 @@ public class CredentialController {
                                           @RequestParam(required = false) String credentialType,
                                           @RequestParam(required = false) String name,
                                           @RequestParam(required = false) String dniBeneficiary,
-                                  //        @RequestParam(required = false) String creditHolderDni,
+                                          @RequestParam(required = false) String creditHolderDni,
                                           @RequestParam(required = false) String idDidiCredential,
                                           @RequestParam(required = false) String lastUpdate,
                                           @RequestParam(required = false) List<String> credentialState) {
@@ -59,7 +59,7 @@ public class CredentialController {
         Page<CredentialDto> credentials;
         try {
             log.info("find credentials "+credentialType+" "+name+dniBeneficiary+" "+idDidiCredential+" "+lastUpdate+" "+credentialState.get(0));
-            credentials = credentialService.findCredentials(credentialType, name, dniBeneficiary, null /*creditHolderDni*/, idDidiCredential, lastUpdate, credentialState, page);
+            credentials = credentialService.findCredentials(credentialType, name, dniBeneficiary, creditHolderDni, idDidiCredential, lastUpdate, credentialState, page);
         } catch (Exception e) {
             log.info("There has been an error searching for credentials with the filters "+ credentialType + " " + name + " " + dniBeneficiary + " " + idDidiCredential + " " +
                     credentialState.toString() + " " + e);

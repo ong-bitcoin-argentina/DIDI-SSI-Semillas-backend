1be424182bb52a812d3b1a8c5dcf29a42b54141f
Martín Luoni <mluoni@atixlabs.com>
2020-07-20 10:19:45 -0300

    feat(config) config deploy on production enviroment

--- src/main/java/com/atixlabs/semillasmiddleware/security/configuration/DBInitializer.java
+++ src/main/java/com/atixlabs/semillasmiddleware/security/configuration/DBInitializer.java
@@ -14,6 +14,7 @@ import com.atixlabs.semillasmiddleware.app.repository.CredentialStateRepository;
 import com.atixlabs.semillasmiddleware.app.repository.ParameterConfigurationRepository;
 import com.atixlabs.semillasmiddleware.app.repository.RevocationReasonRepository;
 import com.atixlabs.semillasmiddleware.security.dto.UserEditRequest;
+import com.atixlabs.semillasmiddleware.security.exceptions.ExistUserException;
 import com.atixlabs.semillasmiddleware.security.model.Role;
 import com.atixlabs.semillasmiddleware.security.repository.PermissionRepository;
 import com.atixlabs.semillasmiddleware.security.model.Permission;
@@ -28,6 +29,7 @@ import java.util.Optional;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -51,8 +53,10 @@ public class DBInitializer implements CommandLineRunner {
 
     private ProcessControlRepository processControlRepository;
 
+    private String enviroment;
+
     @Autowired
-    public DBInitializer(UserService userService, RoleService roleService, PermissionRepository permissionRepository, MenuRepository menuRepository, CredentialStateRepository credentialStateRepository, ParameterConfigurationRepository parameterConfigurationRepository, RevocationReasonRepository revocationReasonRepository, ProcessControlRepository processControlRepository) {
+    public DBInitializer(UserService userService, RoleService roleService, PermissionRepository permissionRepository, MenuRepository menuRepository, CredentialStateRepository credentialStateRepository, ParameterConfigurationRepository parameterConfigurationRepository, RevocationReasonRepository revocationReasonRepository, ProcessControlRepository processControlRepository, @Value("${deploy.enviroment}") String enviroment) {
         this.userService = userService;
         this.roleService = roleService;
         this.permissionRepository = permissionRepository;
@@ -61,6 +65,7 @@ public class DBInitializer implements CommandLineRunner {
         this.parameterConfigurationRepository = parameterConfigurationRepository;
         this.revocationReasonRepository = revocationReasonRepository;
         this.processControlRepository = processControlRepository;
+        this.enviroment = enviroment;
     }
 
     @Override
@@ -157,7 +162,98 @@ public class DBInitializer implements CommandLineRunner {
                                     .permissions(Sets.newHashSet(modifyUserPermission, viewProfilePermission))
                                     .build());
         }
-//TODO change pass
+
+        if(enviroment!=null && enviroment.equals("production")){
+            this.createProductionUsers();
+        }else
+            this.createUsers();
+
+        Optional<Menu> menu = menuRepository.findByCode("USERS");
+        if (menu.isPresent() && menu.get().getName().contains("Profile")) {
+            menu.get().setName("My account");
+            menuRepository.save(menu.get());
+        }
+
+        if (menuRepository.findByCode("VIEW_PROFILE").isPresent()) {
+            menuRepository.save(menu.get());
+        }
+
+        //Credential States
+        Optional<CredentialState> credentialStateOptional = credentialStateRepository.findByStateName(CredentialStatesCodes.CREDENTIAL_ACTIVE.getCode());
+        if (credentialStateOptional.isEmpty()) {
+            credentialStateRepository.save(new CredentialState(CredentialStatesCodes.CREDENTIAL_ACTIVE.getCode()));
+        }
+        Optional<CredentialState> credentialStateRevoke = credentialStateRepository.findByStateName(CredentialStatesCodes.CREDENTIAL_REVOKE.getCode());
+        if (credentialStateRevoke.isEmpty()) {
+            credentialStateRepository.save(new CredentialState(CredentialStatesCodes.CREDENTIAL_REVOKE.getCode()));
+        }
+
+        Optional<CredentialState> credentialStatePending = credentialStateRepository.findByStateName(CredentialStatesCodes.PENDING_DIDI.getCode());
+        if (credentialStatePending.isEmpty()) {
+            credentialStateRepository.save(new CredentialState(CredentialStatesCodes.PENDING_DIDI.getCode()));
+        }
+
+        if(!parameterConfigurationRepository.findByConfigurationName(ConfigurationCodes.MAX_EXPIRED_AMOUNT.getCode()).isPresent()){
+            ParameterConfiguration configuration = new ParameterConfiguration();
+            configuration.setValue("10250");
+            configuration.setConfigurationName(ConfigurationCodes.MAX_EXPIRED_AMOUNT.getCode());
+            parameterConfigurationRepository.save(configuration);
+        }
+
+        if(!parameterConfigurationRepository.findByConfigurationName(ConfigurationCodes.ID_DIDI_ISSUER.getCode()).isPresent()){
+            ParameterConfiguration configuration = new ParameterConfiguration();
+            configuration.setValue("1234567890");
+            configuration.setConfigurationName(ConfigurationCodes.ID_DIDI_ISSUER.getCode());
+            parameterConfigurationRepository.save(configuration);
+        }
+
+
+        //revocation reasons
+        if(revocationReasonRepository.findAll().size() == 0){
+            RevocationReason updateReason = new RevocationReason(RevocationReasonsCodes.UPDATE_INTERNAL.getCode());
+            revocationReasonRepository.save(updateReason);
+
+            RevocationReason cancelledReason = new RevocationReason(RevocationReasonsCodes.CANCELLED.getCode());
+            revocationReasonRepository.save(cancelledReason);
+
+
+            RevocationReason expiredReason = new RevocationReason(RevocationReasonsCodes.EXPIRED_INFO.getCode());
+            revocationReasonRepository.save(expiredReason);
+
+            RevocationReason unlinkingReason = new RevocationReason(RevocationReasonsCodes.UNLINKING.getCode());
+            revocationReasonRepository.save(unlinkingReason);
+
+            RevocationReason defaultReason = new RevocationReason(RevocationReasonsCodes.DEFAULT.getCode());
+            revocationReasonRepository.save(defaultReason);
+        }
+
+        if(processControlRepository.findByProcessName(ProcessNamesCodes.BONDAREA.getCode()).isEmpty()){
+            ProcessControl process = new ProcessControl();
+            process.setProcessName(ProcessNamesCodes.BONDAREA.getCode());
+            process.setStatus(ProcessControlStatusCodes.OK.getCode());
+            processControlRepository.save(process);
+        }
+        if(processControlRepository.findByProcessName(ProcessNamesCodes.CREDENTIALS.getCode()).isEmpty()){
+            ProcessControl process = new ProcessControl();
+            process.setProcessName(ProcessNamesCodes.CREDENTIALS.getCode());
+            process.setStatus(ProcessControlStatusCodes.OK.getCode());
+            //set an initial time (then will be use to compare)
+            process.setStartTime(DateUtil.getLocalDateTimeNowWithFormat("yyyy-MM-dd HH:mm:ss"));
+            processControlRepository.save(process);
+        }
+        if(processControlRepository.findByProcessName(ProcessNamesCodes.CHECK_DEFAULTERS.getCode()).isEmpty()){
+            ProcessControl process = new ProcessControl();
+            process.setProcessName(ProcessNamesCodes.CHECK_DEFAULTERS.getCode());
+            process.setStatus(ProcessControlStatusCodes.OK.getCode());
+            //set an initial time (then will be use to compare)
+            process.setStartTime(DateUtil.getLocalDateTimeNowWithFormat("yyyy-MM-dd HH:mm:ss"));
+            processControlRepository.save(process);
+        }
+
+    }
+
+    private void createUsers() throws ExistUserException {
+        //TODO change pass
         if (!userService.findByUsername("admin@atixlabs.com").isPresent()) {
             UserEditRequest userResponse = new UserEditRequest();
             userResponse.setUsername("admin@atixlabs.com");
@@ -247,87 +343,86 @@ public class DBInitializer implements CommandLineRunner {
             userResponse.setLastName("Viewer");
             userService.createOrEdit(userResponse);
         }
-        Optional<Menu> menu = menuRepository.findByCode("USERS");
-        if (menu.isPresent() && menu.get().getName().contains("Profile")) {
-            menu.get().setName("My account");
-            menuRepository.save(menu.get());
+    }
+
+
+    private void createProductionUsers() throws ExistUserException {
+        //TODO change pass
+        if (!userService.findByUsername("admin").isPresent()) {
+            UserEditRequest userResponse = new UserEditRequest();
+            userResponse.setUsername("admin");
+            userResponse.setEmail("admin@semillas.org");
+            userResponse.setPassword("admin1q2w3e4r");
+            userResponse.setNewPassword("admin1q2w3e4r");
+            userResponse.setConfirmNewPassword("admin1q2w3e4r");
+            userResponse.setRole(com.atixlabs.semillasmiddleware.security.enums.Role.ROLE_ADMIN.role());
+            userResponse.setName("Admin");
+            userResponse.setLastName("Admin");
+            userService.createOrEdit(userResponse);
         }
 
-        if (menuRepository.findByCode("VIEW_PROFILE").isPresent()) {
-            menuRepository.save(menu.get());
+        if (!userService.findByUsername("atixlabs").isPresent()) {
+            UserEditRequest userResponse = new UserEditRequest();
+            userResponse.setUsername("atixlabs");
+            userResponse.setEmail("info@atixlabs.com");
+            userResponse.setPassword("atix1q2w3e4r");
+            userResponse.setNewPassword("atix1q2w3e4r");
+            userResponse.setConfirmNewPassword("atix1q2w3e4r");
+            userResponse.setRole(com.atixlabs.semillasmiddleware.security.enums.Role.ROLE_ADMIN.role());
+            userResponse.setName("Atix");
+            userResponse.setLastName("Labs");
+            userService.createOrEdit(userResponse);
         }
 
-        //Credential States
-        Optional<CredentialState> credentialStateOptional = credentialStateRepository.findByStateName(CredentialStatesCodes.CREDENTIAL_ACTIVE.getCode());
-        if (credentialStateOptional.isEmpty()) {
-            credentialStateRepository.save(new CredentialState(CredentialStatesCodes.CREDENTIAL_ACTIVE.getCode()));
-        }
-        Optional<CredentialState> credentialStateRevoke = credentialStateRepository.findByStateName(CredentialStatesCodes.CREDENTIAL_REVOKE.getCode());
-        if (credentialStateRevoke.isEmpty()) {
-            credentialStateRepository.save(new CredentialState(CredentialStatesCodes.CREDENTIAL_REVOKE.getCode()));
-        }
-
-        Optional<CredentialState> credentialStatePending = credentialStateRepository.findByStateName(CredentialStatesCodes.PENDING_DIDI.getCode());
-        if (credentialStatePending.isEmpty()) {
-            credentialStateRepository.save(new CredentialState(CredentialStatesCodes.PENDING_DIDI.getCode()));
+        if (!userService.findByUsername("tamara").isPresent()) {
+            UserEditRequest userResponse = new UserEditRequest();
+            userResponse.setUsername("tamara@semillas.com");
+            userResponse.setEmail("tamara@semillas.com");
+            userResponse.setPassword("tama1q2w3e4r");
+            userResponse.setNewPassword("tama1q2w3e4r");
+            userResponse.setConfirmNewPassword("tama1q2w3e4r");
+            userResponse.setRole(com.atixlabs.semillasmiddleware.security.enums.Role.ROLE_ADMIN.role());
+            userResponse.setName("Tamara");
+            userResponse.setLastName("Semillas");
+            userService.createOrEdit(userResponse);
         }
 
-        if(!parameterConfigurationRepository.findByConfigurationName(ConfigurationCodes.MAX_EXPIRED_AMOUNT.getCode()).isPresent()){
-            ParameterConfiguration configuration = new ParameterConfiguration();
-            configuration.setValue("10250");
-            configuration.setConfigurationName(ConfigurationCodes.MAX_EXPIRED_AMOUNT.getCode());
-            parameterConfigurationRepository.save(configuration);
+        if (!userService.findByUsername("cronUser@atixlabs.com").isPresent()) {
+            UserEditRequest userResponse = new UserEditRequest();
+            userResponse.setUsername("cronUser@atixlabs.com");
+            userResponse.setEmail("cronUser@atixlabs.com");
+            userResponse.setPassword("cron1q2w3e4r");
+            userResponse.setNewPassword("cron1q2w3e4r");
+            userResponse.setConfirmNewPassword("cron1q2w3e4r");
+            userResponse.setRole(com.atixlabs.semillasmiddleware.security.enums.Role.ROLE_ADMIN.role());
+            userResponse.setName("cronUser");
+            userResponse.setLastName("cronUser");
+            userService.createOrEdit(userResponse);
         }
 
-        if(!parameterConfigurationRepository.findByConfigurationName(ConfigurationCodes.ID_DIDI_ISSUER.getCode()).isPresent()){
-            ParameterConfiguration configuration = new ParameterConfiguration();
-            configuration.setValue("1234567890");
-            configuration.setConfigurationName(ConfigurationCodes.ID_DIDI_ISSUER.getCode());
-            parameterConfigurationRepository.save(configuration);
+        if (!userService.findByUsername("didiUser@atixlabs.com").isPresent()) {
+            UserEditRequest userResponse = new UserEditRequest();
+            userResponse.setUsername("didiUser@atixlabs.com");
+            userResponse.setEmail("didiUser@atixlabs.com");
+            userResponse.setPassword("didi1q2w3e4r");
+            userResponse.setNewPassword("didi1q2w3e4r");
+            userResponse.setConfirmNewPassword("didi1q2w3e4r");
+            userResponse.setRole(com.atixlabs.semillasmiddleware.security.enums.Role.ROLE_ADMIN.role());
+            userResponse.setName("didiUser");
+            userResponse.setLastName("didiUser");
+            userService.createOrEdit(userResponse);
         }
-
-
-        //revocation reasons
-        if(revocationReasonRepository.findAll().size() == 0){
-            RevocationReason updateReason = new RevocationReason(RevocationReasonsCodes.UPDATE_INTERNAL.getCode());
-            revocationReasonRepository.save(updateReason);
-
-            RevocationReason cancelledReason = new RevocationReason(RevocationReasonsCodes.CANCELLED.getCode());
-            revocationReasonRepository.save(cancelledReason);
-
-
-            RevocationReason expiredReason = new RevocationReason(RevocationReasonsCodes.EXPIRED_INFO.getCode());
-            revocationReasonRepository.save(expiredReason);
-
-            RevocationReason unlinkingReason = new RevocationReason(RevocationReasonsCodes.UNLINKING.getCode());
-            revocationReasonRepository.save(unlinkingReason);
-
-            RevocationReason defaultReason = new RevocationReason(RevocationReasonsCodes.DEFAULT.getCode());
-            revocationReasonRepository.save(defaultReason);
+        if (!userService.findByUsername("viewer").isPresent()) {
+            UserEditRequest userResponse = new UserEditRequest();
+            userResponse.setUsername("viewer");
+            userResponse.setEmail("viewer@semillas.com");
+            userResponse.setPassword("viewer1q2w3e4r");
+            userResponse.setNewPassword("viewer1q2w3e4r");
+            userResponse.setConfirmNewPassword("viewer1q2w3e4r");
+            userResponse.setRole(com.atixlabs.semillasmiddleware.security.enums.Role.ROLE_VIEWER.role());
+            userResponse.setName("Viewer");
+            userResponse.setLastName("Viewer");
+            userService.createOrEdit(userResponse);
         }
-
-        if(processControlRepository.findByProcessName(ProcessNamesCodes.BONDAREA.getCode()).isEmpty()){
-            ProcessControl process = new ProcessControl();
-            process.setProcessName(ProcessNamesCodes.BONDAREA.getCode());
-            process.setStatus(ProcessControlStatusCodes.OK.getCode());
-            processControlRepository.save(process);
-        }
-        if(processControlRepository.findByProcessName(ProcessNamesCodes.CREDENTIALS.getCode()).isEmpty()){
-            ProcessControl process = new ProcessControl();
-            process.setProcessName(ProcessNamesCodes.CREDENTIALS.getCode());
-            process.setStatus(ProcessControlStatusCodes.OK.getCode());
-            //set an initial time (then will be use to compare)
-            process.setStartTime(DateUtil.getLocalDateTimeNowWithFormat("yyyy-MM-dd HH:mm:ss"));
-            processControlRepository.save(process);
-        }
-        if(processControlRepository.findByProcessName(ProcessNamesCodes.CHECK_DEFAULTERS.getCode()).isEmpty()){
-            ProcessControl process = new ProcessControl();
-            process.setProcessName(ProcessNamesCodes.CHECK_DEFAULTERS.getCode());
-            process.setStatus(ProcessControlStatusCodes.OK.getCode());
-            //set an initial time (then will be use to compare)
-            process.setStartTime(DateUtil.getLocalDateTimeNowWithFormat("yyyy-MM-dd HH:mm:ss"));
-            processControlRepository.save(process);
-        }
-
     }
 }

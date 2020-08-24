980881aff692efd645fd0ec69c3c239c5c2d6761
Eric Stoppel <estoppel@atixlabs.com>
2020-08-24 11:10:24 -0300

    feat: add validations

--- src/main/java/com/atixlabs/semillasmiddleware/app/model/identityValidationRequest/dto/IdentityValidationRequestDto.java
+++ src/main/java/com/atixlabs/semillasmiddleware/app/model/identityValidationRequest/dto/IdentityValidationRequestDto.java
@@ -3,14 +3,20 @@ package com.atixlabs.semillasmiddleware.app.model.identityValidationRequest.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 public class IdentityValidationRequestDto {
+    @NotNull
     private Long dni;
+    @NotNull
     private String did;
     private String email;
     private String phone;
+    @NotNull
     private String name;
+    @NotNull
     private String lastName;
     private String revocationReason;
 }

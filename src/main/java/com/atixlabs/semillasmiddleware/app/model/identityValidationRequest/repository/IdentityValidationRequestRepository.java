e4e8b46b95667b63b884bc7b9b1206b13c7c7441
Eric Stoppel <estoppel@atixlabs.com>
2020-08-31 11:24:42 -0300

    feat: add specification filters to identity requests

--- src/main/java/com/atixlabs/semillasmiddleware/app/model/identityValidationRequest/repository/IdentityValidationRequestRepository.java
+++ src/main/java/com/atixlabs/semillasmiddleware/app/model/identityValidationRequest/repository/IdentityValidationRequestRepository.java
@@ -2,6 +2,7 @@ package com.atixlabs.semillasmiddleware.app.model.identityValidationRequest.repo
 
 import com.atixlabs.semillasmiddleware.app.model.identityValidationRequest.model.IdentityValidationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IdentityValidationRequestRepository extends JpaRepository<IdentityValidationRequest, Long> {
+public interface IdentityValidationRequestRepository extends JpaRepository<IdentityValidationRequest, Long>, JpaSpecificationExecutor<IdentityValidationRequest> {
 }

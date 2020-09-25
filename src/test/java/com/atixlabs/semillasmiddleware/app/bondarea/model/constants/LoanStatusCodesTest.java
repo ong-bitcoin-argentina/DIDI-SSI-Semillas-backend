b246b14a1533a08f866f063d42aadc52405e4097
Eric Stoppel <estoppel@atixlabs.com>
2020-09-25 15:17:43 -0300

    feat: send credit state description to didi issuer

---
+++ src/test/java/com/atixlabs/semillasmiddleware/app/bondarea/model/constants/LoanStatusCodesTest.java
@@ -0,0 +1,19 @@
+package com.atixlabs.semillasmiddleware.app.bondarea.model.constants;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanStatusCodesTest {
+
+    @Test
+    void whenFinalizedString_getByCode_expectFinalizedEnum() {
+        String finalizedCode = LoanStatusCodes.FINALIZED.getCode();
+        Optional<LoanStatusCodes> code = LoanStatusCodes.getByCode(finalizedCode);
+        assertTrue(code.isPresent());
+        assertEquals(code.get(), LoanStatusCodes.FINALIZED);
+    }
+}

b8bed2de09d006ea9fceebf1c129570b2c62e740
abarreto1 <abarreto@atixlabs.com>
2020-11-16 09:45:26 -0300

    feat: didi credential creation rollback

---
+++ src/main/java/com/atixlabs/semillasmiddleware/app/exceptions/DidiEmmitCredentialException.java
@@ -0,0 +1,10 @@
+package com.atixlabs.semillasmiddleware.app.exceptions;
+
+public class DidiEmmitCredentialException extends Exception {
+    public DidiEmmitCredentialException(String s) {
+        super(s);
+    }
+    public DidiEmmitCredentialException(Exception e) {
+        super(e);
+    }
+}

ec5e26457c7edfab8f1a88c1350fca930a00cda5
abarreto1 <abarreto@atixlabs.com>
2020-11-19 17:15:45 -0300

    fix: changing ArrayList for List in credentialRepository

--- src/main/java/com/atixlabs/semillasmiddleware/app/repository/CredentialRepository.java
+++ src/main/java/com/atixlabs/semillasmiddleware/app/repository/CredentialRepository.java
@@ -17,9 +17,9 @@ public interface CredentialRepository extends JpaRepository<Credential, Long> , 
 
     ArrayList<Credential> findByCredentialCategoryAndCredentialState(String code, CredentialState credentialStatePending);
 
-    ArrayList<Credential> findByBeneficiaryDniAndCredentialCategoryAndCreditHolderDniAndCredentialStateIn(Long beneficiaryDni, String credentialCategoryCode, Long creditHolderDni, List<CredentialState> credentialStateActivePending);
+    List<Credential> findByBeneficiaryDniAndCredentialCategoryAndCreditHolderDniAndCredentialStateIn(Long beneficiaryDni, String credentialCategoryCode, Long creditHolderDni, List<CredentialState> credentialStateActivePending);
 
-    ArrayList<Credential> findByBeneficiaryDniAndCredentialCategoryAndCredentialStateIn(Long beneficiaryDni, String credentialCategoryCode, List<CredentialState> credentialStateActivePending);
+    List<Credential> findByBeneficiaryDniAndCredentialCategoryAndCredentialStateIn(Long beneficiaryDni, String credentialCategoryCode, List<CredentialState> credentialStateActivePending);
 
     ArrayList<Credential> findByCreditHolderDniIn(List<Long> dniList);
 

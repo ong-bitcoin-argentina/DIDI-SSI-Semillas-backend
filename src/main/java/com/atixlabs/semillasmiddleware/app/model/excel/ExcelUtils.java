ba957b378bfa6e4dfc32236aa9087cb36d310eb0
Ese <silvia.oxalde@didi.org.ar>
2022-04-12 22:11:23 -0300

    fix loop de credenciales revocadas  SEM-123

--- src/main/java/com/atixlabs/semillasmiddleware/app/model/excel/ExcelUtils.java
+++ src/main/java/com/atixlabs/semillasmiddleware/app/model/excel/ExcelUtils.java
@@ -1,6 +1,12 @@
 package com.atixlabs.semillasmiddleware.app.model.excel;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
 import java.util.Date;
+import java.util.Objects;
 
 public class ExcelUtils {
     //Relacionadas a la vivienda
@@ -12,6 +18,24 @@ public class ExcelUtils {
     public static String formatAddress(Form form){
         return (form.getActividadDirCalle()+" N° "+form.getActividadDirNumero()+" e/ "+form.getActividadDirEntreCalles()+", "+
                 form.getActividadDirBarrio()+", "+form.getActividadDirLocalidad()+", "+form.getActividadDirMunicipio());
+    }
+
+    public static void formatHeader(XSSFSheet sheet) {
+        if (Objects.isNull(sheet))
+            return;
+        XSSFRow headerRow =  sheet.getRow(0);
+        for(int i=0;i<headerRow.getLastCellNum() ;i++){
+            XSSFCell cell = headerRow.getCell(i);
+            boolean hasTwoHash = StringUtils.countMatches(cell.getStringCellValue(), "#")==2;
+            if(hasTwoHash){
+                String[] splittedCellContent = cell.getStringCellValue().split("#");
+                String title = splittedCellContent[1];
+                String subTitle = splittedCellContent[2].substring(splittedCellContent[2].lastIndexOf("/"));
+                subTitle = subTitle.replaceAll("/span>", "").replaceAll("/", "_"); // clean string
+                headerRow.createCell(i);
+                headerRow.getCell(i).setCellValue(title + subTitle);
+            }
+        }
     }
 
 }
